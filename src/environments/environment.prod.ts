1a62e85f211c40dfba69c2ea319d1950b4f3adb4
Almamy-Samina <almamy.haidara@samina.tech>
2025-03-10 11:38:22 +0000

    kkkk*

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment={
+export const environment = {
   production: true,
-  apiUrl: 'https://app-rfid-back-ffawhne8fdb9ese6.francecentral-01.azurewebsites.net/api'
+  apiUrl:
+    'https://app-rfid-back-ffawhne8fdb9ese6.centralus-01.azurewebsites.net/api',
+  // apiUrl: 'https://app-rfid-back-ffawhne8fdb9ese6.francecentral-01.azurewebsites.net/api'
 };
